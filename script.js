9130ccdbef47b67fe37f3de8cf7bb08d152df018
Preetam Gupta <106731801+PreetamGup@users.noreply.github.com>
2023-01-25 19:35:52 +0530

    latest

--- script.js
+++ script.js
@@ -14,3 +14,44 @@ if (close) {
         nav.classList.remove('active')
     })
 }
+
+
+// redirecting to Single product page information when click on product
+
+const product = document.getElementsByClassName("pro");
+// console.log(product);
+
+let imgurl="";
+for(let item of product){
+    item.addEventListener("click", ()=>{
+
+        imgurl= item.children[0].src;
+           console.log(imgurl);
+        //  displaySingleProduct(imgurl)
+
+        window.location.href='sproduct.html';
+      
+    })
+}
+
+
+
+
+
+
+
+
+
+// console.log(imgurl)
+
+// const sproductPage= document.getElementById('prodetails');
+
+// function displaySingleProduct(){
+//     console.log("im herer")
+//     const MainImg=document.getElementById("MainImg");
+//         console.log(MainImg.src);
+//         console.log(imgurl);
+       
+// }
+
+
